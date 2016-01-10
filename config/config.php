6baa1f5afa53ab43e4d93a73a2553a396d00dfb1
Robert <robert.gol.jr@gmail.com>
2016-01-10 11:56:47 -0200

    padrão mvc

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    define ('DEVELOPMENT_ENVIRONMENT',true);
+
+    define('DB_NAME', 'naive');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'g741237');
+    define('DB_HOST', 'localhost');
